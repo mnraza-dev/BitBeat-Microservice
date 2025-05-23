e18f1edd7fbff115423e5f10d25bb798d68d3465
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-23 23:19:53 +0530

    improved development workflow

--- UserService/src/index.ts
+++ UserService/src/index.ts
@@ -6,7 +6,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 app.listen(port, () => {
-    
+
     console.log(`Server running at http://localhost:${port}`);
 });
