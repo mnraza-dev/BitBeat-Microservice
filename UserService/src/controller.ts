6e229e8c112b364e7cdc41a89b1227eb6b0f07b0
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-24 00:13:53 +0530

    refactor: enhance registerUser function with improved error handling and user creation logic

--- UserService/src/controller.ts
+++ UserService/src/controller.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from "express";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import TryCatch from "./TryCatch";
-export const registerUser = TryCatch(async (req: Request, res: Response) => {
+import User from "./model.js";
+
+
+export const registerUser = TryCatch(async (req, res) => {
+    const { name, email, password } = req.body
+    let user = await User.findOne({ email });
+    if (user) {
+        res.status(400).json({
+            message: "User already exists"
+        })
+        return;
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await User.create({ name, email, password: hashedPassword });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, { expiresIn: "7d" });
     res.json({
-        message: "User registered successfully"
+        message: "User registered successfully",
+        data: {
+            user,
+            token
+        }
     })
 })
 export const loginUser =
