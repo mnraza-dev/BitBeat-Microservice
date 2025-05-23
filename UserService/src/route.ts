62c77ffbec010209d02642be05b10aba1d7c34e5
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-24 03:52:07 +0530

    feat: add route for user profile retrieval with authentication

--- UserService/src/route.ts
+++ UserService/src/route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "./controller";
+import { loginUser, myProfile, registerUser } from "./controller";
+import { isAuth } from "./middleware";
 
 const router = Router();
-
 router.post('/user/register', registerUser)
 router.post('/user/login', loginUser)
+router.get('/user/profile',isAuth, myProfile)
 
 export default router
