7889d9340557dcf1f2c907cc1b45d5a3c8361f6a
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-23 23:27:20 +0530

    establish database connection on server start

--- UserService/src/index.ts
+++ UserService/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from '../config/db';
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 app.listen(port, () => {
-
+connectDB();
     console.log(`Server running at http://localhost:${port}`);
 });
