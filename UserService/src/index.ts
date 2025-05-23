951a7633b9520f25dee36d4379c9d8673e7c8be9
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-23 23:13:26 +0530

    initialize user service with express and dotenv; add type definitions for dependencies

---
+++ UserService/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(express.json());
+
+app.listen(port, () => {
+    
+    console.log(`Server running at http://localhost:${port}`);
+});
